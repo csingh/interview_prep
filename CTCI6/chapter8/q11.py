67219afbc3a0db128c9a8dac4c2fe154c8fb8416
Chandeep Singh <s.chandeep@gmail.com>
2016-04-03 21:38:11 -0400

    remove memoization for now and add some comments

--- CTCI6/chapter8/q11.py
+++ CTCI6/chapter8/q11.py
@@ -1,30 +1,32 @@
-def numWays(n):
-    memo = dict()
+# TODO: add back memoization?
+#       -originally removed because the memo key was only using
+#        n which is wrong. Need to use n and coins
 
-    def numWaysUtil(n, coins):
-        if n < 0: return 0
-        if n == 0 or not coins: return 1
-        
-        if n in memo:
-            return memo[n]
+def numWays(n, coins):
+    # can't represent a negative number
+    if n < 0: return 0
 
-        ways = 0
-        coin = coins.pop(0)
-        numCoins = 0
-        while (coin*numCoins <= n):
-            ways += numWaysUtil(n - coin*numCoins, coins[:])
-            numCoins += 1
+    # if we reached n == 0 then we found a combination that works
+    # or if coins is empty, then we can just represent n with only pennies
+    if n == 0 or not coins: return 1
+    
+    ways = 0
+    coin = coins.pop(0) # get a coin
+    numCoins = 0
+    
+    # recurse with using the coin in all the different ways possible:
+    #       e.g. for n = 75 and coins = [25,10,5]
+    #            we will take coin = 25c, and we can use it either
+    #            once, twice, or three times. Then recurse with the
+    #            remaining types of coins [10,5]
+    while (coin*numCoins <= n):
+        ways += numWays(n - coin*numCoins, coins[:])
+        numCoins += 1
 
-        memo[n] = ways
-
-        return ways
-
-    ans = numWaysUtil(n, [25,10,5])
-
-    return ans
+    return ways
 
 def test(n, expected):
-    ans = numWays(n)
+    ans = numWays(n, coins=[25,10,5])
     if ans == expected:
         print("    Correct :", end="");
     else:
@@ -48,4 +50,3 @@ if __name__ == '__main__':
         test(x,9)
 
     test(25,13)
-    print(numWays(10000))
