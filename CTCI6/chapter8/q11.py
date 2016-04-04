1b0c03f73c6141eb96cd4ca4e6375318e5cea716
Chandeep Singh <s.chandeep@gmail.com>
2016-04-03 21:48:35 -0400

    update comments

--- CTCI6/chapter8/q11.py
+++ CTCI6/chapter8/q11.py
@@ -1,32 +1,32 @@
-# TODO: add back memoization?
-#       -originally removed because the memo key was only using
-#        n which is wrong. Need to use n and coins
+AVAILABLE_COINS = [25,10,5]
 
 def numWays(n, coins):
+    coins = coins[:]
     # can't represent a negative number
     if n < 0: return 0
 
     # if we reached n == 0 then we found a combination that works
-    # or if coins is empty, then we can just represent n with only pennies
+    # or if coins is empty, then we're only left with pennies to represent n
     if n == 0 or not coins: return 1
     
+    # set up le vars
     ways = 0
     coin = coins.pop(0) # get a coin
     numCoins = 0
     
-    # recurse with using the coin in all the different ways possible:
+    # recurse using the coin in all the number of times possible:
     #       e.g. for n = 75 and coins = [25,10,5]
     #            we will take coin = 25c, and we can use it either
-    #            once, twice, or three times. Then recurse with the
-    #            remaining types of coins [10,5]
+    #            once, twice, or three times. In each recursive call,
+    #            we only use the remaining types of coins [10,5]
     while (coin*numCoins <= n):
-        ways += numWays(n - coin*numCoins, coins[:])
+        ways += numWays(n - coin*numCoins, coins)
         numCoins += 1
 
     return ways
 
 def test(n, expected):
-    ans = numWays(n, coins=[25,10,5])
+    ans = numWays(n,AVAILABLE_COINS)
     if ans == expected:
         print("    Correct :", end="");
     else:
@@ -50,3 +50,4 @@ if __name__ == '__main__':
         test(x,9)
 
     test(25,13)
+    test(1000,-1)
