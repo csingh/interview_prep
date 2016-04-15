0038ab7128fc0071050a60c56e7869817f594de0
Chandeep Singh <s.chandeep@gmail.com>
2016-04-15 17:05:56 -0400

    add simple Graph class

---
+++ Graph.py
@@ -0,0 +1,44 @@
+class Graph:
+    def __init__(self):
+        self.adj_dict = dict()
+        self.vertices = []
+
+    def add_directed_edge(self, src_v, dest_v):
+        # add vertices to vertices array
+        if src_v not in self.vertices:
+            self.vertices.append(src_v)
+        if dest_v not in self.vertices:
+            self.vertices.append(dest_v)
+
+        # add edge to adjacency dictionary
+        if src_v in self.adj_dict:
+            if dest_v not in self.adj_dict[src_v]:
+                self.adj_dict[src_v].append(dest_v)
+        else:
+            self.adj_dict[src_v] = [dest_v]
+
+    def add_undirected_edge(self, vert_1, vert_2):
+        self.add_directed_edge(vert_1, vert_2)
+        self.add_directed_edge(vert_2, vert_1)
+
+    def __str__(self):
+        vertices = sorted(self.vertices)
+        s = "Graph Edges:"
+
+        for v in vertices:
+            connected = []
+
+            if v in self.adj_dict:
+                connected = sorted(self.adj_dict[v])
+
+            s += "\n{} -> {}".format(v, connected)
+
+        return s
+
+if __name__ == '__main__':
+    g = Graph()
+    g.add_undirected_edge("A","B")
+    g.add_directed_edge("A", "C")
+    g.add_directed_edge("A", "B")
+    g.add_directed_edge("D", "C")
+    print(g)
